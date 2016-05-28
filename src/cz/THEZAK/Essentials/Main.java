4825c0e8e9c4ef4f53f33e2a058209021029c0a3
THEZAKCZ <fivecz15@gmail.com>
2016-05-28 18:45:00 +0200

    Add files via upload

--- src/cz/THEZAK/Essentials/Main.java
+++ src/cz/THEZAK/Essentials/Main.java
@@ -3,13 +3,14 @@ package cz.THEZAK.Essentials;
 import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
-import cz.THEZAK.Essentials.commands.Commands;
+import cz.THEZAK.Essentials.commands.*;
 import java.io.File;
 import java.util.LinkedHashMap;
 
 public class Main extends PluginBase
   implements Listener
 {
+
 @SuppressWarnings({ "deprecation", "unchecked", "serial", "rawtypes" })
 public void onEnable()
   {
@@ -19,14 +20,28 @@ public void onEnable()
       getDataFolder(), "config.yml"), 2, new LinkedHashMap()
     {
     });
-    new Commands();
     config.save();
-
     getServer().getLogger().info("EssentialsPE loaded!");
+    getServer().getCommandMap().register("setspawn", new setSpawn(this));
+    getServer().getCommandMap().register("spawn", new spawn(this));
+    getServer().getCommandMap().register("warp", new warp(this));
+    getServer().getCommandMap().register("setwarp", new setWarp(this));
+    getServer().getCommandMap().register("day", new day(this));
+    getServer().getCommandMap().register("night", new night(this));
+    getServer().getCommandMap().register("rain", new sun(this));
+    getServer().getCommandMap().register("sun", new rain(this));
+    getServer().getCommandMap().register("delwarp", new delWarp(this));
+    getServer().getCommandMap().register("sethome", new setHome(this));
+    getServer().getCommandMap().register("home", new home(this));
+    getServer().getCommandMap().register("delhome", new home(this));
+    getServer().getCommandMap().register("suicide", new suicide(this));
+    getServer().getCommandMap().register("heal", new heal(this));
+    getServer().getCommandMap().register("clear", new clear(this));
   }
-    
     public void onDisable()
     {
       getServer().getLogger().info("EssentialsPE disabled!");
   }
 }
+
+// Created by THEZAK :)
